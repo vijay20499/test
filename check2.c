5422f42c8ada26dd159bd0d01ddbb7ab8f723d15
vijay20499 <51689378+vijay20499@users.noreply.github.com>
2019-06-11 10:20:40 +0530

    Add files via upload

---
+++ check2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+char a;
+scanf("%c",&a);
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+printf("Vowel");
+else if(a>=97&&a<=122)
+printf("Consonant");
+else
+printf("invalid");
+}
