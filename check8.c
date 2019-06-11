b1daccda65da5c8d5a4df823e0612bcfd911f5f1
vijay20499 <51689378+vijay20499@users.noreply.github.com>
2019-06-11 11:43:05 +0530

    Update check8.c

--- check8.c
+++ check8.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 int main()
 {
-int n,m,sum=0,a[1000];
-scanf("%d %d",&n,&m);
-for(int i=0;i<n;i++)
-scanf("%d",&a[i]);
-for(int i=0;i<m;i++)
-sum=sum+a[i];
-printf("%d",sum);
+  int n;
+  scanf("%d",&n);
+  int count=0;
+  while(n>0)
+  {
+    count++;
+    n=n/10;
+  }
+  printf("%d",count);
 }
